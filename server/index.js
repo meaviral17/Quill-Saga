22c41f2a55fa2cd385677096718802eb48c25b93
mohdshahzil <theshahzil@gmail.com>
2024-01-31 04:28:10 +0530

    authentication-and-authorization

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 /*CONFIGURATION*/
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -34,6 +35,12 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
+
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES
+app.use("/auth", authRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
