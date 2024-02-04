3bbab060e649f4d744d2af5e5da4c1f1b75175e0
mohdshahzil <theshahzil@gmail.com>
2024-02-05 03:24:48 +0530

    React Redux File Folder Architecture and React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage></LoginPage>} />
+          <Route path="/home" element={<HomePage></HomePage>} />
+          <Route
+            path="/profile/:userId"
+            element={<ProfilePage></ProfilePage>}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
