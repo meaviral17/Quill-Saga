cc7b6aaa755c067dc31c27e25d7bca1a4bcad9db
mohdshahzil <theshahzil@gmail.com>
2024-02-06 03:01:07 +0530

    Color Theme Dark Mode and Styling Setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,28 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "./theme";
 const App = () => {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage></LoginPage>} />
-          <Route path="/home" element={<HomePage></HomePage>} />
-          <Route
-            path="/profile/:userId"
-            element={<ProfilePage></ProfilePage>}
-          />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage></LoginPage>} />
+            <Route path="/home" element={<HomePage></HomePage>} />
+            <Route
+              path="/profile/:userId"
+              element={<ProfilePage></ProfilePage>}
+            />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
